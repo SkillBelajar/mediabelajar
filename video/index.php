d7a1fdfb9a666b096da34235bafcbecab0749b45
SkillBelajar <80496983+SkillBelajar@users.noreply.github.com>
2022-10-20 07:59:04 +0700

    1

---
+++ video/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<title>Video Pembelajaran</title>
+
+<body>
+
+    <video width="900" height="600" controls>
+        <source src="file.mp4" type="video/mp4">
+
+
+    </video>
+
+</body>
+
+</html>
